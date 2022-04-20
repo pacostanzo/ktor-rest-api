73beab86fa72d1f150401a43e2e58a97b32651b1
Pablo Costanzo <costanzopa@gmail.com>
2022-04-20 11:43:02 -0300

    Adding Order routes

--- src/main/kotlin/com/costanzopa/plugins/Routing.kt
+++ src/main/kotlin/com/costanzopa/plugins/Routing.kt
@@ -1,11 +1,17 @@
 package com.costanzopa.plugins
 
 import com.costanzopa.routes.customerRouting
+import com.costanzopa.routes.getOrderRoute
+import com.costanzopa.routes.listOrdersRoute
+import com.costanzopa.routes.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
