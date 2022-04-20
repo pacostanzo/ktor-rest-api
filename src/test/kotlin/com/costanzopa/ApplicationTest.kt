01cb471829d0499c912c9d9ad97e18dac3e8ae43
Pablo Costanzo <costanzopa@gmail.com>
2022-04-20 11:53:44 -0300

    Adding get order test

--- src/test/kotlin/com/costanzopa/ApplicationTest.kt
+++ src/test/kotlin/com/costanzopa/ApplicationTest.kt
@@ -1,27 +1,20 @@
 package com.costanzopa
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
-import io.ktor.serialization.gson.*
-import io.ktor.server.plugins.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import com.costanzopa.plugins.*
 
 class ApplicationTest {
+
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
+    fun testGetOrder() = testApplication {
+        val response = client.get("/order/2020-04-06-01")
+        assertEquals(
+            """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 }
