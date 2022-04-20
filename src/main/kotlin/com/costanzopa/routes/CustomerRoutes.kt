166debcb44f5c8b0267e2a99b4f1d54ae45de8fd
Pablo Costanzo <costanzopa@gmail.com>
2022-04-20 00:18:07 -0300

    Adding customers endpoints.

--- src/main/kotlin/com/costanzopa/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/costanzopa/routes/CustomerRoutes.kt
@@ -1,8 +1,10 @@
 package com.costanzopa.routes
 
+import com.costanzopa.models.Customer
 import com.costanzopa.models.customerStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -15,14 +17,30 @@ fun Route.customerRouting() {
                 call.respondText("No customers found", status = HttpStatusCode.OK)
             }
         }
-    }
-    get("{id?}") {
-
-    }
-    post {
-
-    }
-    delete("{id?}") {
-
+        get("{id?}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpStatusCode.NotFound
+                )
+            call.respond(customer)
+        }
+        post {
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
+        }
+        delete("{id?}") {
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id }) {
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            }
+        }
     }
 }
